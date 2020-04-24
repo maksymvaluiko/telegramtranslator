596f030bdf3f0af6d38a639360ed39c701b450d2
Maksym Valuiko <maksym.valuiko@gmail.com>
2020-04-24 17:54:39 +0300

    bot

---
+++ Cognitive/ImageToText.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace CognitiveServices
+{
+    public class ImageToText
+    {
+        const string telegramKeyToken = "1208156096:AAGEDobyvScgd3L-SbSVrCtb3TgufscYFcA";
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        // Retuns array of words.
+        public static async Task<string[]> RecognizePrintedTextUrl(ComputerVisionClient client, string imageUrl)
+        {
+            // Perform OCR on image
+            OcrResult remoteOcrResult = null;
+
+            try
+            {
+                remoteOcrResult = await client.RecognizePrintedTextAsync(true, imageUrl);
+            }
+            catch(Exception ex)
+            {
+                string s = ex.Message;
+            }
+
+            List<string> result = new List<string>();
+
+            foreach (var remoteRegion in remoteOcrResult.Regions)
+            {
+                foreach (var line in remoteRegion.Lines)
+                {
+                    result.Add(string.Join(' ', line.Words.Select(w => w.Text)));
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public static async Task ResponceToUser()
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Cognitive/Translation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cognitive.Trans
+{
+    public class TranslationResult
+    {
+        public DetectedLanguage DetectedLanguage { get; set; }
+        public TextResult SourceText { get; set; }
+        public Translation[] Translations { get; set; }
+    }
+
+    public class DetectedLanguage
+    {
+        public string Language { get; set; }
+        public float Score { get; set; }
+    }
+
+    public class TextResult
+    {
+        public string Text { get; set; }
+        public string Script { get; set; }
+    }
+
+    public class Translation
+    {
+        public string Text { get; set; }
+        public TextResult Transliteration { get; set; }
+        public string To { get; set; }
+        public Alignment Alignment { get; set; }
+        public SentenceLength SentLen { get; set; }
+    }
+
+    public class Alignment
+    {
+        public string Proj { get; set; }
+    }
+
+    public class SentenceLength
+    {
+        public int[] SrcSentLen { get; set; }
+        public int[] TransSentLen { get; set; }
+    }
+
+    public class TranslationServcie
+    {
+        private const string key_var = "TRANSLATOR_TEXT_SUBSCRIPTION_KEY";
+        private static readonly string subscriptionKey = "c3caeeaaa1c84117bfd7d5e9786e2326";
+
+        private const string endpoint_var = "TRANSLATOR_TEXT_ENDPOINT";
+        private static readonly string endpoint = "https://api.cognitive.microsofttranslator.com";
+
+
+        public static async Task<string> TranslateSentense(string sentense, string lang)
+        {
+            string route = $"/translate?api-version=3.0&to={lang}";
+
+            return await TranslateTextRequest(subscriptionKey, endpoint, route, sentense);
+        }
+
+        public static async Task<string> TranslateTextRequest(string subscriptionKey, string endpoint, string route, string inputText)
+        {
+            string resultTrans = string.Empty;
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(endpoint + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+                TranslationResult[] deserializedOutput = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+                // Iterate over the deserialized results.
+                foreach (TranslationResult o in deserializedOutput)
+                {
+                    // Print the detected input languge and confidence score.
+                    Console.WriteLine("Detected input language: {0}\nConfidence score: {1}\n", o.DetectedLanguage.Language, o.DetectedLanguage.Score);
+                    // Iterate over the results and print each translation.
+                    foreach (Translation t in o.Translations)
+                    {
+                        resultTrans += t.Text;
+                    }
+                }
+            }
+
+            return resultTrans;
+        }
+
+    }
+}
